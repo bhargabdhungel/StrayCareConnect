03080ae014857eeb7865aeef833e8ce15a1a095a
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-04-10 21:47:11 +0530

    backend init

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
