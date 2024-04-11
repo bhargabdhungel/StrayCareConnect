37281cb2d27b25f9686c7bcfc91b452f84d37939
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-04-11 13:58:22 +0530

    frontend init

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { config } from "dotenv";
 import cors from "cors";
+import connectDB from "./database/index.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>Welcome to my app</p>
+    </div>
+  );
+}
