fd18d6c68457f7c48ae13a8c4539fd47b81a5ea9
Rohan G <2001.rohangupta@gmail.com>
2024-04-12 19:58:38 +0530

    get all organizations

--- backend/routes/user/index.js
+++ backend/routes/user/index.js
@@ -4,11 +4,13 @@ import me from "./me.js";
 import addPost from "./addPost.js";
 import Post from "../../models/post.js";
 import getPost from "./getPost.js";
+import getAllOrgs from "./getAllOrgs.js";
 
 const router = express.Router();
 
 router.get("/me", verifyUser, me);
 router.post("/post", verifyUser, addPost);
 router.get("/posts", verifyUser, getPost);
+router.get("/allOrgs", verifyUser, getAllOrgs)
 
 export default router;
