6d86da7281afef61e65358c01da2ae03680ef451
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-04-12 17:09:55 +0530

    org models

---
+++ backend/models/orgPost.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const postSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    gender: { type: String, required: true },
+    image: { type: String, required: true },
+    age: { type: Number, required: true },
+    animalType: {
+      type: String,
+      enum: ["Dog", "Cat", "Cow", "Bird", "Horse"],
+      required: true,
+    },
+    description: { type: String, required: true },
+    monthlyBudget: { type: Number, required: true },
+    orgId: { type: mongoose.Schema.Types.ObjectId, ref: "OrgUser" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("OrgPost", postSchema);
+export default Post;
