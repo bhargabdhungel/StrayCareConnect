006ac773692b6d0a116d0738640a23ac576d8620
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-04-12 17:34:04 +0530

    paged posts

---
+++ backend/routes/post/getPost.js
@@ -0,0 +1,21 @@
+import Post from "../../models/post.js";
+
+const getPostController = async (req, res) => {
+  try {
+    const pageSize = 10;
+    const skip = (req.params.page - 1) * pageSize;
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip(skip) 
+      .limit(pageSize);
+    return posts;
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export default getPostController;
